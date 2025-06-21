c9122200370d72f9684408ac24e72ad2ebe34c3a
jack <jacklinuxnd@gmail.com>
2025-06-21 17:28:20 +0530

    ..

--- apps/front/src/routes.tsx
+++ apps/front/src/routes.tsx
@@ -3,7 +3,13 @@
 export const routes = [
   {
     path: '/',
-    element: <>home</>,
+    element: <><div className="">
+         <div className=" bg-blue-600 text-white flex items-center justify-center text-3xl font-bold">
+      ✅ Tailwind is working!
+    </div>
+        
+        
+        </div></>,
     label: 'Home',
   },
   {
